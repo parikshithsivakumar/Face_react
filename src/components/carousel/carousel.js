19a840fb375969160d8de1ee92e0e10e1151d048
Kallepalli Rahul Varma <rahulvarma2584@gmail.com>
2024-06-11 22:51:28 +0530

    changes made

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,11 +1,10 @@
-// SimpleCarousel.js
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './SimpleCarousel.css';
-import img2 from '../../static/imgs/new_hero.png';
-import img3 from '../../static/imgs/php.png';
+import img1 from '../../static/imgs/events_1.jpg';
+import img2 from '../../static/imgs/events_2.jpg';
 
 const SimpleCarousel = () => {
   const settings = {
@@ -14,33 +13,29 @@ const SimpleCarousel = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true, // Enable auto play
+    autoplaySpeed: 3000, // Auto play speed
+    arrows: false, // Hide navigation arrows for a cleaner look
   };
+
   return (
     <div className="carousel-container">
-      <h1 style={{ color: "#5aefe7", paddingLeft: 20  }}>Highlights</h1>
+      <h1 className="carousel-heading">Highlights</h1>
       <Slider {...settings}>
         <div className="carousel-slide">
-          <img src={img2} alt="Slide 1" />
+          <img src={img1} alt="Slide 1" />
           <div className="slide-content">
-            <h3>Subheading 1</h3>
-            <p>This is some normal content for Slide 1.</p>
+            <h2>React Workshop</h2>
+            <p>Experience the world of React with this opportunity</p>
           </div>
         </div>
         <div className="carousel-slide">
           <img src={img2} alt="Slide 2" />
           <div className="slide-content">
-            <h3>Subheading 2</h3>
-            <p>This is some normal content for Slide 2.</p>
+            <h2>Hands-on React Workshop</h2>
+            <p>Experience the world of React with this opportunity</p>
           </div>
         </div>
-        <div className="carousel-slide">
-          <img src={img3} alt="Slide 3" />
-          <div className="slide-content">
-            <h3>Subheading 3</h3>
-            <p>This is some normal content for Slide 3.</p>
-          </div>
-        </div>
-        {/* Add more slides as needed */}
       </Slider>
     </div>
   );
