ad84c0d697b52bb2e58d270e4b666f1de1a11a47
Kallepalli Rahul Varma <rahulvarma2584@gmail.com>
2024-07-03 12:24:30 +0530

    changes made

--- src/pages/aboutus/contactus.js
+++ src/pages/aboutus/contactus.js
@@ -11,19 +11,22 @@ const ContactUs = () => {
             If you have any questions, suggestions, or if you want to get involved with FACE Club, feel free to reach out to us. We are always open to new ideas and collaborations.
           </p>
           <p>Email: faceclub@example.com</p>
+          <p>Phone No.: 1234567890</p>
         </div>
         <form className="contact-form">
-          <div className="form-field">
+          <div className="form-field1">
+            <div  className="form-field">
             <label htmlFor="name">Name:</label>
             <input type="text" id="name" name="name" />
-          </div>
-          <div className="form-field">
+            </div>
+            <div  className="form-field">
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" />
+            </div>
           </div>
           <div className="form-field">
             <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" rows="4" />
+            <textarea id="message" name="message" rows="4"  />
           </div>
           <button type="submit" className="submit-button">Send Message</button>
         </form>
