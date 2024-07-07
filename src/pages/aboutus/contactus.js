da16be002073bdf14755673a4b9ff518d5534daa
Rohit Nair <rohitsnair2004@gmail.com>
2024-07-07 12:33:09 +0530

    changes made

--- src/pages/aboutus/contactus.js
+++ src/pages/aboutus/contactus.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Contactus.css';
+import React from "react";
+import "./Contactus.css";
 
 const ContactUs = () => {
   return (
@@ -8,40 +8,51 @@ const ContactUs = () => {
         <h2 className="contact-header">Contact Us</h2>
         <div className="contact-info">
           <p>
-            If you have any questions, suggestions, or if you want to get involved with FACE Club, feel free to reach out to us. We are always open to new ideas and collaborations.
+            If you have any questions, suggestions, or if you want to get
+            involved with FACE Club, feel free to reach out to us. We are always
+            open to new ideas and collaborations.
           </p>
           <p>Email: faceclub@example.com</p>
           <p>Phone No.: 1234567890</p>
         </div>
         <form className="contact-form">
           <div className="form-field1">
-            <div  className="form-field1">
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
+            <div className="form-field1">
+              <label htmlFor="name">Name:</label>
+              <input type="text" id="name" name="name" />
             </div>
-            <div  className="form-field1">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" />
+            <div className="form-field1">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" />
             </div>
           </div>
           <div className="form-field">
             <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" rows="3"  />
+            <textarea id="message" name="message" rows="3" />
           </div>
-          <button type="submit" className="submit-button">Send Message</button>
+          <button type="submit" className="submit-button">
+            Send Message
+          </button>
         </form>
         <div className="social-icons">
-          <a href="#" className="social-icon"><i className="fab fa-instagram"></i></a>
-          <a href="#" className="social-icon"><i className="fab fa-twitter"></i></a>
-          <a href="#" className="social-icon"><i className="fab fa-facebook"></i></a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fab fa-facebook"></i>
+          </a>
         </div>
       </div>
       <div className="map-container">
         <iframe
+          title="Location"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.2000010277447!2d77.6731963597311!3d12.894857487361003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1303337c1ea7%3A0x8d48e3c7dd1e0b50!2sAmrita%20Vishwa%20Vidyapeetham%2C%20Bengaluru!5e0!3m2!1sen!2sin!4v1717739019275!5m2!1sen!2sin"
           width="100%"
           height="100%"
-          style={{border:0}}
+          style={{ border: 0 }}
           allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
@@ -49,6 +60,6 @@ const ContactUs = () => {
       </div>
     </div>
   );
-}
+};
 
 export default ContactUs;
