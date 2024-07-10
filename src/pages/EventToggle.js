ad95dc0edecf13ebe55add5661e6debbced29445
Kallepalli Rahul Varma <rahulvarma2584@gmail.com>
2024-07-10 22:47:18 +0530

    changes made events details update

--- src/pages/EventToggle.js
+++ src/pages/EventToggle.js
@@ -10,32 +10,34 @@ const EventToggle = () => {
 
   const events = [
     {
-      title: "Event 1",
+      title: "Hogwarts Hackfest",
       date: "2023-11-20",
-      description: "Brief details about Event 1.",
+      description: "Immersing participants in the enchanting world of Harry Potter, the event infused coding challenges with the magic and wonder of J.K. Rowling's universe, creating a unique and captivating atmosphere.",
       venue: "Amrita School of Engineering in Bengaluru ",
       time: "November 21, 2023, to November 26, 2023",
       registrationLink: "https://example.com/register/event1",
-      image: "https://via.placeholder.com/150", // Add the URL of the event image
+      readmoreLink: "https://example.com/register/event2",
+      image: "https://media.licdn.com/dms/image/D5622AQGslPUxu6lRrA/feedshare-shrink_1280/0/1718303482503?e=1723680000&v=beta&t=ngSjVS3wjuAcpkqMvhEJBfEBj1ebsLMYZtRlydQ0iSw", // Add the URL of the event image
     },
     {
-      title: "Event 2",
-      date: "2024-03-26",
-      description: "Brief details about Event 2.",
-      venue: "Venue 2",
-      time: "02:00 PM",
+      title: "Algomania",
+      date: "2023-11-24",
+      description: "Algomania is a Competitive Programming contest where participants compete in groups of three or four. The one-of-a-kind event had tough twists like the Blind Coding Round, in which participants solved puzzles without the use of a visual aid, and the quick-witted Relay Coding Round, which tested teamwork under time limits.",
+      venue: "CS Labs",
+      time: "9am - 12pm",
       registrationLink: "https://example.com/register/event2",
-      image: "https://via.placeholder.com/150", // Add the URL of the event image
+      readmoreLink: "https://example.com/register/event2",
+      image: "https://media.licdn.com/dms/image/D5622AQGbgddMq83Fqw/feedshare-shrink_2048_1536/0/1720546428077?e=1723680000&v=beta&t=4Z8zTb5s9tBInZkRRYiBelNVM0Y_10YXZkXyiJgKPKM", // Add the URL of the event image
     },
     {
-      title: "Event 3",
+      title: "Hands On Workshop On React",
       date: "2024-02-03",
-      description:
-        "Brief details about Event 3.A workshop on react happened where our alumini from 24/7ai company came as speakers of the workshop",
+      description:"A big shoutout to our distinguished alumni, T N Varunram and Vishal Raman, for leading this engaging session. Their expertise on HTML/CSS basics and React’s core concepts, including architecture and state management, was invaluable. They provided deep insights into full-stack development, covering UI/UX, frontend, backend integration, and databases.",
       venue: "Venue 3",
       time: "04:00 PM",
       registrationLink: "https://example.com/register/event3",
-      image: "https://via.placeholder.com/150", // Add the URL of the event image
+      readmoreLink: "https://example.com/register/event3",
+      image: "https://media.licdn.com/dms/image/D5622AQGAQWOmC0Ilug/feedshare-shrink_2048_1536/0/1718196472028?e=1723680000&v=beta&t=mxXBGrNVcaLW4QDq1exQiytEFHcHiZMa4yQBw1CIqBE", // Add the URL of the event image
     },
     {
       title: "Event 4",
@@ -44,6 +46,7 @@ const EventToggle = () => {
       venue: "Venue 4",
       time: "04:00 PM",
       registrationLink: "https://example.com/register/event4",
+      readmoreLink: "https://example.com/register/event2",
       image: "https://via.placeholder.com/150", // Add the URL of the event image
     },
     // Add more events here
@@ -99,7 +102,15 @@ const EventToggle = () => {
                     >
                       Register Here
                     </a>
-                    <button className="read-more-button">Read More</button>
+                    <a
+                      href={event.readmoreLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="read-more-button"
+                    >
+                      Read More
+                    </a>
+                    {/* <button className="read-more-button">Read More</button> */}
                   </div>
                 </div>
                 <div className="event-image">
