be9898744b8596d3d0cae3e31b979f0c9d51e638
Kallepalli Rahul Varma <rahulvarma2584@gmail.com>
2024-07-10 23:49:22 +0530

    changes made

--- src/pages/EventToggle.js
+++ src/pages/EventToggle.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./events.css"; // Create a CSS file for styling
+import facereact from "../static/imgs/reactworkshop.jpeg"
+import algomania from "../static/imgs/algomania.jpeg"
+import hogwarts from "../static/imgs/hogwarts.jpeg"
+
 
 const EventToggle = () => {
   const [view, setView] = useState("all");
@@ -12,38 +16,32 @@ const EventToggle = () => {
     {
       title: "Hogwarts Hackfest",
       date: "2023-11-20",
-      description:
-        "Immersing participants in the enchanting world of Harry Potter, the event infused coding challenges with the magic and wonder of J.K. Rowling's universe, creating a unique and captivating atmosphere.",
+      description:"Immersing participants in the enchanting world of Harry Potter, the event infused coding challenges with the magic and wonder of J.K. Rowling's universe, creating a unique and captivating atmosphere.",
       venue: "Amrita School of Engineering in Bengaluru ",
       time: "November 21, 2023, to November 26, 2023",
       registrationLink: "https://example.com/register/event1",
       readmoreLink: "https://example.com/register/event2",
-      image:
-        "https://media.licdn.com/dms/image/D5622AQGslPUxu6lRrA/feedshare-shrink_1280/0/1718303482503?e=1723680000&v=beta&t=ngSjVS3wjuAcpkqMvhEJBfEBj1ebsLMYZtRlydQ0iSw", // Add the URL of the event image
+      image:hogwarts, // Add the URL of the event image
     },
     {
       title: "Algomania",
       date: "2023-11-24",
-      description:
-        "Algomania is a Competitive Programming contest where participants compete in groups of three or four. The one-of-a-kind event had tough twists like the Blind Coding Round, in which participants solved puzzles without the use of a visual aid, and the quick-witted Relay Coding Round, which tested teamwork under time limits.",
+      description:"Algomania is a Competitive Programming contest where participants compete in groups of three or four. The one-of-a-kind event had tough twists like the Blind Coding Round, in which participants solved puzzles without the use of a visual aid, and the quick-witted Relay Coding Round, which tested teamwork under time limits.",
       venue: "CS Labs",
       time: "9am - 12pm",
       registrationLink: "https://example.com/register/event2",
       readmoreLink: "https://example.com/register/event2",
-      image:
-        "https://media.licdn.com/dms/image/D5622AQGbgddMq83Fqw/feedshare-shrink_2048_1536/0/1720546428077?e=1723680000&v=beta&t=4Z8zTb5s9tBInZkRRYiBelNVM0Y_10YXZkXyiJgKPKM", // Add the URL of the event image
+      image:algomania, // Add the URL of the event image
     },
     {
       title: "Hands On Workshop On React",
       date: "2024-02-03",
-      description:
-        "A big shoutout to our distinguished alumni, T N Varunram and Vishal Raman, for leading this engaging session. Their expertise on HTML/CSS basics and React’s core concepts, including architecture and state management, was invaluable. They provided deep insights into full-stack development, covering UI/UX, frontend, backend integration, and databases.",
+      description:"A big shoutout to our distinguished alumni, T N Varunram and Vishal Raman, for leading this engaging session. Their expertise on HTML/CSS basics and React’s core concepts, including architecture and state management, was invaluable. They provided deep insights into full-stack development, covering UI/UX, frontend, backend integration, and databases.",
       venue: "Venue 3",
       time: "04:00 PM",
       registrationLink: "https://example.com/register/event3",
       readmoreLink: "https://example.com/register/event3",
-      image:
-        "https://media.licdn.com/dms/image/D5622AQGAQWOmC0Ilug/feedshare-shrink_2048_1536/0/1718196472028?e=1723680000&v=beta&t=mxXBGrNVcaLW4QDq1exQiytEFHcHiZMa4yQBw1CIqBE", // Add the URL of the event image
+      image:facereact, // Add the URL of the event image
     },
     {
       title: "Event 4",
